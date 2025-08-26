495210e039b221c80bdfad47de068ed5609dd4ff
bsenoz <bsenoz@hotmail.com>
2025-08-26 13:48:13 +0300

    dns

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,3 @@
-<script setup>
-useHead({
-  meta: [
-    {
-      name: 'google-site-verification',
-      content: 'epKX6hjwmDUpIaezqXWT78hi78PnKfPejR-T3MdmBcY'
-    }
-  ]
-})
-</script>
-
 <template>
     <CommonProgressBar />
     <IntroHeroSection />
